06a8e47a5013b96b3bd0b021144d26ad9c47100e
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2018-10-24 18:44:32 +0100

    Add remote-state example

---
+++ Lesson4/remote-state/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "mybucketname"
+    key     = "mytfstate"
+    region  = "us-west-2"
+    profile = "env"
+  }
+}
---
+++ Lesson4/remote-state/terraform.tfvars
@@ -0,0 +1,6 @@
+profile = "env"
+subnet_id = "subnet-12345"
+tags = {
+  Name                 = "tf-course"
+  Team                 = "Team Name"
+}
